f61f24b7377761104e1aecd446ab255c8e7d846c
mario malak <93185168+mariomalak1@users.noreply.github.com>
2024-12-10 04:57:28 +0200

    tasks per day done

--- SRC/Controllers/task.controller.js
+++ SRC/Controllers/task.controller.js
@@ -1,3 +1,5 @@
+import {Op} from "sequelize";
+
 import {TaskModel} from "../../DB/Models/task.model.js";
 import {responseObjWithPaginator} from "../Utillis/defaultResponses.js";
 import {paginate} from "../Utillis/paginationForModel.js";
@@ -110,5 +112,57 @@ export const numberOfComUnComTasks = async (req, res, next) => {
     });
 
     return res.status(200).json({data: {"numOfCompletedTasks": numOfCompletedTasks.length, "numOfUnCompletedTasks": numOfUnCompletedTasks.length}});
+}
+
+export const numberOfTasksCompeletedPerDayInMonth = async (req, res, next) => {
+    let {month} = req.query;
+    
+    if(!month){
+        return res.status(400).json({"error": "must provide month number"});
+    }
+
+    if(Number.isNaN(month) && (month > 11 || month < 0)){
+        return res.status(400).json({"error": "must provide valid month number"});
+    }
+
+    const currentYear = new Date().getFullYear();
+
+    month -= 1;
+    
+    let greaterThanData, smallerThanData;
+
+    if(month == 0){
+        greaterThanData = new Date(currentYear - 1, 11, 1);
+    }else{
+        greaterThanData = new Date(currentYear, month, 1);
+
+    }
+
+    if(month == 11){
+        smallerThanData = new Date(currentYear + 1, 0, 1);        
+    }else{
+        smallerThanData = new Date(currentYear, month, 1);
+    }
+
+
+    const tasks = await TaskModel.findAll({
+        where: {
+            status: 1,
+            completeTime: {
+                [Op.gte]: greaterThanData,
+                [Op.lt]: smallerThanData,
+            }
+        }
+    });
+
+    const numOfDaysInMonth = new Date(currentYear, 5, 0).getDate();
+
+    const tasksPerDay = {};
+
+    for (let i = 1; i < numOfDaysInMonth; i++) {
+        tasksPerDay[`day_${i}`] = tasks.filter((task) => task.completeTime.getDate() === i);        
+    }
+
+    return res.status(200).json({data: {tasksPerDay}});
 }
 
--- SRC/Routers/task.router.js
+++ SRC/Routers/task.router.js
@@ -1,8 +1,9 @@
 import {Router} from "express";
 
 import {getAllTasksForUser, getTaskWithId,
-     createTask, updateTask, deleteTask,
-     numberOfComUnComTasks} from "../Controllers/task.controller.js";
+    createTask, updateTask, deleteTask,
+    numberOfComUnComTasks,
+    numberOfTasksCompeletedPerDayInMonth} from "../Controllers/task.controller.js";
 import {verifyToken} from "../Utillis/userToken.js";
 
 export const router = Router();
@@ -17,3 +18,4 @@ router.route("/:id")
     .delete(verifyToken, deleteTask);
 
 router.get("/statistics/num/", verifyToken, numberOfComUnComTasks)
+router.get("/statistics/nTasksInMonth/", verifyToken, numberOfTasksCompeletedPerDayInMonth)
